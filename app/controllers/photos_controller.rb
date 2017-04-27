be492fd005077dd5af6c5f498daa3d3a3781cf46
Eddie Jou <eddiejou@users.noreply.github.com>
2017-04-27 18:51:49 -0500

    HW Changes Made

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,44 @@
+class PhotosController <ApplicationController
+
+  def index
+    @list_of_photos=Photo.all
+    render("/photos/index.html.erb")
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+    render("/photos/show.html.erb")
+  end
+
+  def new_form
+    render("/photos/new_form.html.erb")
+  end
+
+  def create_row
+    photo = Photo.new
+    photo.caption = params[:the_caption]
+    photo.source = params[:the_source]
+    photo.save
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find(params[:id])
+    render("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    photo = Photo.find(params[:id])
+    photo.caption = params[:the_caption]
+    photo.source = params[:the_source]
+    photo.save
+    redirect_to("/photos/#{photo.id}")
+  end
+
+  def destroy_row
+    photo = Photo.find(params[:id])
+    photo.destroy
+    redirect_to("/photos")
+  end
+
+end
